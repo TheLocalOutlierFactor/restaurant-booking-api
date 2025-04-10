e48d517d0590518481eb7ed0dfd8edc7d9bb7be9
TheLocalOutlierFactor <localoutlierfactor@gmail.com>
2025-04-10 21:29:19 +0500

    Add logger and favicon

--- src/main.py
+++ src/main.py
@@ -1,6 +1,31 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+from src.utils.logger import logger
+
 
 app = FastAPI(title="Restaurant Table Booking API")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    logger.info(f"➡️ {request.method} {request.url}")
+    start_time = time.time()
+
+    response = await call_next(request)
+
+    process_time = time.time() - start_time
+    logger.info(f"✅ {request.method} {request.url} completed in {process_time:.2f}s with {response.status_code}")
+    return response
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("static/favicon.ico")
 
 
 @app.get("/")
---
+++ src/utils/logger.py
@@ -0,0 +1,15 @@
+import logging
+
+
+logger = logging.getLogger("restaurant_booking")
+logger.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    "[%(asctime)s] [%(levelname)s] %(message)s"
+)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
