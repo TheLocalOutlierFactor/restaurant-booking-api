2ac246b6b6b961fba19a71d75fba62074d730e80
TheLocalOutlierFactor <localoutlierfactor@gmail.com>
2025-04-12 20:20:24 +0500

    Add more tests, add unique constraint handler on Table

--- src/services/table_service.py
+++ src/services/table_service.py
@@ -1,8 +1,12 @@
+from fastapi import HTTPException
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 
 from src.models.table import Table
 from src.schemas.table import TableCreate
+from src.utils.error_messages import TABLE_EXISTS
 
 
 async def get_all_tables(db: AsyncSession):
@@ -13,7 +17,17 @@ async def get_all_tables(db: AsyncSession):
 async def create_table(db: AsyncSession, table: TableCreate):
     db_table = Table(**table.model_dump())
     db.add(db_table)
-    await db.commit()
+    try:
+        await db.commit()
+    except IntegrityError as e:
+        if e.orig.sqlstate == "23505":  # Ошибка 23505 это Unique Constraint Violation
+            await db.rollback()
+            raise HTTPException(
+                status_code=409,
+                detail=TABLE_EXISTS
+            )
+        await db.rollback()
+        raise  # Ошибка все равно бросается, если она не связана с дубликатом имени
     await db.refresh(db_table)
     return db_table
 
--- tests/test_table.py
+++ tests/test_table.py
@@ -1,16 +1,73 @@
 import pytest
 
+from src.utils.error_messages import TABLE_EXISTS
+
+
+def create_table(name="Столик 1", seats=2, location="главный зал"):
+    table = {
+        "name": name,
+        "seats": seats,
+        "location": location,
+    }
+    return table
+
 
 @pytest.mark.asyncio
 async def test_create_table(client):
-    response = await client.post("/tables/", json={
-        "name": "Столик 1",
-        "seats": 2,
-        "location": "У камина"
-    })
+    table = create_table()
+
+    response = await client.post("/tables/", json=table)
     data = response.json()
 
     assert response.status_code == 201
-    assert data["name"] == "Столик 1"
-    assert data["seats"] == 2
-    assert data["location"] == "У камина"
+    assert data["name"] == table["name"]
+    assert data["seats"] == table["seats"]
+    assert data["location"] == table["location"]
+
+
+@pytest.mark.asyncio
+async def test_get_tables(client):
+    table_1 = create_table(name="Столик 1", seats=2, location="главный зал")
+    table_2 = create_table(name="Столик 2", seats=4, location="терраса")
+
+    await client.post("/tables/", json=table_1)
+    await client.post("/tables/", json=table_2)
+
+    response = await client.get("/tables/")
+    data = response.json()
+
+    assert response.status_code == 200
+    assert len(data) == 2
+
+
+@pytest.mark.asyncio
+async def test_table_unique_constrain(client):
+    table_1 = create_table(name="Столик 1", seats=2, location="главный зал")
+    table_2 = create_table(name="Столик 1", seats=4, location="терраса")
+
+    response_success = await client.post("/tables/", json=table_1)
+    response_clash = await client.post("/tables/", json=table_2)
+
+    assert response_success.status_code == 201
+    assert response_clash.status_code == 409
+    assert response_clash.json()["detail"] == TABLE_EXISTS
+
+
+@pytest.mark.asyncio
+async def test_remove_table(client):
+    table_1 = create_table(name="Столик 1", seats=2, location="главный зал")
+    table_2 = create_table(name="Столик 2", seats=4, location="терраса")
+
+    create_table_1_response = await client.post("/tables/", json=table_1)
+    create_table_2_response = await client.post("/tables/", json=table_2)
+
+    created_table_1_id = create_table_1_response.json()["id"]
+    created_table_2_id = create_table_2_response.json()["id"]
+
+    delete_table_1_response = await client.delete(f"/tables/{created_table_1_id}")
+    delete_table_2_response = await client.delete(f"/tables/{created_table_2_id}")
+    delete_missing_table_response = await client.delete(f"/tables/{created_table_1_id}")
+
+    assert delete_table_1_response.status_code == 200
+    assert delete_table_2_response.status_code == 200
+    assert delete_missing_table_response.status_code == 404
